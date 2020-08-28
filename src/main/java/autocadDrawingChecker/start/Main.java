2b191484567523308d661c43119c6e48707e6764
IronHeart7334 <greengrappler12@gmail.com>
2020-08-28 10:58:51 -0700

    For some reason, git won't let me exclude every file under build except for the JAR, so I have to manually ignore each other folder.

--- src/main/java/autocadDrawingChecker/start/Main.java
+++ src/main/java/autocadDrawingChecker/start/Main.java
@@ -5,5 +5,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Application runs properly");
     }
 }
