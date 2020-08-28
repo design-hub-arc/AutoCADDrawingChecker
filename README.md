0bb91f452ca52a598308fc021b73a4fc27083632
IronHeart7334 <greengrappler12@gmail.com>
2020-08-28 11:22:22 -0700

    There we go! Finally got the Excel library up and running!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AutoCADDrawingChecker
  TODO
+
+## Helpful Links
+* [Excel Library](https://poi.apache.org/apidocs/4.1/)
--- src/main/java/autocadDrawingChecker/start/Main.java
+++ src/main/java/autocadDrawingChecker/start/Main.java
@@ -1,10 +1,20 @@
 package autocadDrawingChecker.start;
 
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.extractor.XSSFExcelExtractor;
+
 public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        InputStream in = Main.class.getResourceAsStream("/testWorkbook.xlsx");
+        System.out.println(in);
+        XSSFWorkbook workbook = new XSSFWorkbook(in);
+        
+        XSSFExcelExtractor reader = new XSSFExcelExtractor(workbook);
         System.out.println("Application runs properly");
     }
 }
