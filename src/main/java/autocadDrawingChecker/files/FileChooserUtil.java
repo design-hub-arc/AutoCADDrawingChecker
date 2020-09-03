31491895797a0d56c53f1b8708d6f75758891f16
IronHeart7334 <greengrappler12@gmail.com>
2020-09-03 09:29:48 -0700

    I don't quite like the file chooser util, but I don't want to spend too much time trying to make it perfect.

--- src/main/java/autocadDrawingChecker/files/FileChooserUtil.java
+++ src/main/java/autocadDrawingChecker/files/FileChooserUtil.java
@@ -2,9 +2,11 @@ package autocadDrawingChecker.files;
 
 import autocadDrawingChecker.logging.Logger;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.function.Consumer;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -13,14 +15,37 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  * @author Matt
  */
 public class FileChooserUtil {
-    public static void askCreateTextFile(String dialogTitle, String textFileContents){
-        JFileChooser chooser = new JFileChooser();
+    private final JFileChooser chooser;
+    
+    public FileChooserUtil(String dialogTitle, int dialogType, int fileSelectionMode){
+        chooser = new JFileChooser();
         chooser.setDialogTitle(dialogTitle);
-        chooser.setDialogType(JFileChooser.SAVE_DIALOG);
-        //chooser.setFileFilter(new FileNameExtensionFilter("Text file", new String[]{"txt"}));
-        if(chooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION){
+        chooser.setDialogType(dialogType);
+        chooser.setFileSelectionMode(fileSelectionMode);
+    }
+    
+    public static void askChooseFile(String dialogTitle, FileType allowedType, Consumer<File> andThen){
+        FileChooserUtil fcu = new FileChooserUtil(dialogTitle, JFileChooser.OPEN_DIALOG, JFileChooser.FILES_ONLY);
+        fcu.chooser.setFileFilter(new FileNameExtensionFilter(allowedType.getName(), allowedType.getExtensions()));
+        fcu.chooser.setMultiSelectionEnabled(false);
+        if(fcu.chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+            andThen.accept(fcu.chooser.getSelectedFile());
+        }
+    }
+    
+    public static void askChooseFiles(String dialogTitle, FileType allowedType, Consumer<File[]> andThen){
+        FileChooserUtil fcu = new FileChooserUtil(dialogTitle, JFileChooser.OPEN_DIALOG, JFileChooser.FILES_AND_DIRECTORIES);
+        fcu.chooser.setFileFilter(new FileNameExtensionFilter(allowedType.getName(), allowedType.getExtensions()));
+        fcu.chooser.setMultiSelectionEnabled(true);
+        if(fcu.chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+            andThen.accept(fcu.chooser.getSelectedFiles());
+        }
+    }
+    public static void askCreateTextFile(String dialogTitle, String textFileContents){
+        FileChooserUtil fcu = new FileChooserUtil(dialogTitle, JFileChooser.SAVE_DIALOG, JFileChooser.FILES_AND_DIRECTORIES);
+        if(fcu.chooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION){
             // may move this to a file writer util
-            try (BufferedWriter buff = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(chooser.getSelectedFile())))) {
+            try (BufferedWriter buff = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fcu.chooser.getSelectedFile())))) {
                 buff.write(textFileContents);
             } catch (IOException ex) {
                 Logger.logError(ex);
--- src/main/java/autocadDrawingChecker/gui/ExcelFileChooser.java
+++ src/main/java/autocadDrawingChecker/gui/ExcelFileChooser.java
@@ -1,5 +1,6 @@
 package autocadDrawingChecker.gui;
 
+import autocadDrawingChecker.files.FileChooserUtil;
 import autocadDrawingChecker.files.FileType;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
@@ -7,20 +8,19 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import javax.swing.JButton;
 import javax.swing.JComponent;
-import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
- *
+ * TODO: split into 2 classes
  * @author Matt
  */
 public class ExcelFileChooser extends JComponent implements ActionListener {
     private File selectedFile;
     private final JTextArea text;
-    private final JFileChooser popup;
+    private final String popupTitle;
+    private final boolean allowMultiple;
     
     public ExcelFileChooser(String title, String popupText, boolean allowDir){
         super();
@@ -43,11 +43,8 @@ public class ExcelFileChooser extends JComponent implements ActionListener {
         button.addActionListener(this);
         bottom.add(button);
         add(bottom, BorderLayout.PAGE_END);
-        
-        popup = new JFileChooser();
-        popup.setDialogTitle(popupText);
-        popup.setFileSelectionMode((allowDir) ? JFileChooser.FILES_AND_DIRECTORIES : JFileChooser.FILES_ONLY);
-        popup.setFileFilter(new FileNameExtensionFilter(FileType.EXCEL.getName(), FileType.EXCEL.getExtensions()));
+        popupTitle = popupText;
+        allowMultiple = allowDir;
     }
     
     public final boolean isFileSelected(){
@@ -60,9 +57,16 @@ public class ExcelFileChooser extends JComponent implements ActionListener {
     
     @Override
     public void actionPerformed(ActionEvent e){
-        if(popup.showOpenDialog(this) == JFileChooser.APPROVE_OPTION){
-            selectedFile = popup.getSelectedFile();
-            text.setText("Selected the file " + selectedFile.getAbsolutePath());
+        if(allowMultiple){
+            FileChooserUtil.askChooseFiles(popupTitle, FileType.EXCEL, (File[] fs)->{
+                selectedFile = fs[0];
+                text.setText("Selected the file " + selectedFile.getAbsolutePath());
+            });
+        } else {
+            FileChooserUtil.askChooseFile(popupTitle, FileType.EXCEL, (File f)->{
+                selectedFile = f;
+                text.setText("Selected the file " + selectedFile.getAbsolutePath());
+            });
         }
     }
 }
