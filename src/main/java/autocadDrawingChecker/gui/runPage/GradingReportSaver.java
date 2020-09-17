44ad7db4675f217db38197512d6d909159b89f22
IronHeart7334 <greengrappler12@gmail.com>
2020-09-17 09:59:53 -0700

    Now ensures that the grading report file has an xlsx extension

---
+++ src/main/java/autocadDrawingChecker/gui/runPage/GradingReportSaver.java
@@ -0,0 +1,51 @@
+package autocadDrawingChecker.gui.runPage;
+
+import autocadDrawingChecker.files.FileChooserUtil;
+import autocadDrawingChecker.files.FileType;
+import autocadDrawingChecker.grading.GradingReport;
+import autocadDrawingChecker.logging.Logger;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Consumer;
+
+/**
+ * The GradingReportSaver is a small helper class used to write
+ * a GradingReport to an Excel file.
+ * 
+ * @author Matt Crow
+ */
+public class GradingReportSaver {
+    public static final void saveReport(GradingReport report, Consumer<File> andThen){
+        FileChooserUtil.askChooseFile("Where would you like to save this report to?", FileType.EXCEL, (File f)->{
+            String fStrPath = f.getAbsolutePath();
+            if(!fStrPath.endsWith(".xlsx")){
+                // rename it if it's extension is incorrect
+                Path fPath = f.toPath();
+                String pathStrIWant = fStrPath + ".xlsx";
+                try {                    
+                    if(Files.exists(fPath)){
+                        // rename it if it already exists.
+                        Files.move(fPath, Paths.get(pathStrIWant));
+                    }
+                    // regardless, redirect f to point to this new file path
+                    f = new File(pathStrIWant);
+                } catch(Exception ex){
+                    Logger.logError(ex);
+                }
+            }
+            try (FileOutputStream out = new FileOutputStream(f)) {
+                report.getAsWorkBook().write(out);
+            } catch (FileNotFoundException ex) {
+                Logger.logError(ex);
+            } catch (IOException ex) {
+                Logger.logError(ex);
+            }
+            andThen.accept(f);
+        });
+    }
+}
--- src/main/java/autocadDrawingChecker/gui/runPage/OutputPage.java
+++ src/main/java/autocadDrawingChecker/gui/runPage/OutputPage.java
@@ -1,17 +1,11 @@
 package autocadDrawingChecker.gui.runPage;
 
-import autocadDrawingChecker.files.FileChooserUtil;
-import autocadDrawingChecker.files.FileType;
 import autocadDrawingChecker.grading.GradingReport;
 import autocadDrawingChecker.gui.AbstractPage;
 import autocadDrawingChecker.gui.PageRenderer;
 import autocadDrawingChecker.logging.Logger;
 import autocadDrawingChecker.start.Application;
 import java.awt.GridLayout;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import javax.swing.JButton;
 
 /**
@@ -54,15 +48,10 @@ public class OutputPage extends AbstractPage {
                 GradingReport report = Application.getInstance().grade();
                 Logger.log(report.toString());
         
-                FileChooserUtil.askChooseFile("Where would you like to save this report to?", FileType.EXCEL, (File f)->{
-                    try (FileOutputStream out = new FileOutputStream(f)) {
-                        report.getAsWorkBook().write(out);
-                    } catch (FileNotFoundException ex) {
-                        Logger.logError(ex);
-                    } catch (IOException ex) {
-                        Logger.logError(ex);
-                    }
+                GradingReportSaver.saveReport(report, (savedTo)->{
+                    Logger.log("Successfully saved report to " + savedTo.getAbsolutePath());
                 });
+                
                 run.setEnabled(true);
                 Logger.log("Done grading!");
             }
