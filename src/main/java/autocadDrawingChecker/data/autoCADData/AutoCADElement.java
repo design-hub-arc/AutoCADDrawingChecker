172348597b6ed0934825641d02f264c621f1de47
IronHeart7334 <greengrappler12@gmail.com>
2020-10-13 09:12:26 -0700

    split autocadelement into quasi-abstract and concrete parts

--- src/main/java/autocadDrawingChecker/data/autoCADData/AutoCADElement.java
+++ src/main/java/autocadDrawingChecker/data/autoCADData/AutoCADElement.java
@@ -1,8 +1,6 @@
 package autocadDrawingChecker.data.autoCADData;
 
-import autocadDrawingChecker.logging.Logger;
-import java.util.HashMap;
-import java.util.Set;
+import autocadDrawingChecker.data.core.SpreadsheetRecord;
 
 /**
  * An AutoCADElement represents
@@ -10,19 +8,14 @@ import java.util.Set;
  * 
  * @author Matt Crow
  */
-public class AutoCADElement {
-    private final HashMap<String, Object> attributes;
+public class AutoCADElement extends SpreadsheetRecord {
     
     public static final String NAME_COL = "Name";
     public static final String LAYER_COL = "Layer";
     
     // no access modifier, so only classes in this package can invoke this
     AutoCADElement(){
-        this.attributes = new HashMap<>();
-    }
-    
-    public final Set<String> getAttributes(){
-        return attributes.keySet();
+        super();
     }
     
     /**
@@ -44,79 +37,8 @@ public class AutoCADElement {
         return (String)getAttribute(LAYER_COL);
     }
     
-    private String sanitizeAttributeName(String name){
-        return name.trim().toUpperCase();
-    }
-    
-    final AutoCADElement setAttribute(String attrName, Object value){
-        attributes.put(sanitizeAttributeName(attrName), value);
-        return this;
-    }
-    
-    /**
-     * Checks to see if this has the given attribute
-     * @param attributeName the name to check for
-     * @return whether or not this has the given attribute
-     */
-    public final boolean hasAttribute(String attributeName){
-        return attributes.containsKey(sanitizeAttributeName(attributeName));
-    }
-    
-    /**
-     * Gets this' attribute with the given name.
-     * If this has no such attribute, throws an exception.
-     * You should use getAttributeString(), getAttributeDouble(), or getAttributeInt()
-     * if you want the attribute in another format.
-     * 
-     * @param attributeName the name of the attribute to get.
-     * @return the given attribute as an Object
-     * @throws NullPointerException if this has no attribute with the given name.
-     */
-    public final Object getAttribute(String attributeName) throws NullPointerException {
-        if(!hasAttribute(attributeName)){
-            throw new NullPointerException(String.format("Doesn't have attribute \"%s\"", attributeName));
-        }
-        return attributes.get(sanitizeAttributeName(attributeName));
-    }
-    
-    /**
-     * 
-     * @param attributeName the name of the attribute to get
-     * @return the attribute with the given name
-     * @throws NullPointerException if this has no such attribute with the given name
-     */
-    public final String getAttributeString(String attributeName) throws NullPointerException {
-        return getAttribute(attributeName).toString();
-    }
-    
-    /**
-     * 
-     * @param attributeName the attribute to get
-     * @return the given attribute, as a double
-     * @throws NumberFormatException if the given attribute is not a number
-     */
-    public final double getAttributeDouble(String attributeName) throws NumberFormatException {
-        String s = getAttributeString(attributeName);
-        double ret = 0.0;
-        try {
-            ret = Double.valueOf(s);
-        } catch(NumberFormatException ex){
-            Logger.logError(String.format("Attribute with name \"%s\" cannot be formatted as a number. Its value is \"%s\"", attributeName, s));
-            throw ex;
-        }
-        return ret;
-    }
-    public final int getAttributeInt(String attributeName){
-        return (int)getAttributeDouble(attributeName);
-    }
-    
     @Override
     public String toString(){
-        StringBuilder sb = new StringBuilder();
-        sb.append("AutoCAD Element:");
-        attributes.forEach((k, v)->{
-            sb.append(String.format("\n- %s : %s", k, v.toString()));
-        });
-        return sb.toString();
+        return String.format("AutoCAD Element: \n%s", super.toString());
     }
 }
---
+++ src/main/java/autocadDrawingChecker/data/core/SpreadsheetRecord.java
@@ -0,0 +1,99 @@
+package autocadDrawingChecker.data.core;
+
+import autocadDrawingChecker.logging.Logger;
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * SpreadsheetRecord represents a single row in a spreadsheet.
+ * This class can be extended to provide more specific behavior,
+ * or can be used as is: it is not abstract.
+ * 
+ * @author Matt Crow
+ */
+public class SpreadsheetRecord {
+    private final HashMap<String, Object> attributes;
+    
+    protected SpreadsheetRecord(){
+        this.attributes = new HashMap<>();
+    }
+    
+    public final Set<String> getAttributes(){
+        return attributes.keySet();
+    }
+    
+    private String sanitizeAttributeName(String name){
+        return name.trim().toUpperCase();
+    }
+    
+    public final void setAttribute(String attrName, Object value){
+        attributes.put(sanitizeAttributeName(attrName), value);
+    }
+    
+    /**
+     * Checks to see if this has the given attribute
+     * @param attributeName the name to check for
+     * @return whether or not this has the given attribute
+     */
+    public final boolean hasAttribute(String attributeName){
+        return attributes.containsKey(sanitizeAttributeName(attributeName));
+    }
+    
+    /**
+     * Gets this' attribute with the given name.
+     * If this has no such attribute, throws an exception.
+     * You should use getAttributeString(), getAttributeDouble(), or getAttributeInt()
+     * if you want the attribute in another format.
+     * 
+     * @param attributeName the name of the attribute to get.
+     * @return the given attribute as an Object
+     * @throws NullPointerException if this has no attribute with the given name.
+     */
+    public final Object getAttribute(String attributeName) throws NullPointerException {
+        if(!hasAttribute(attributeName)){
+            throw new NullPointerException(String.format("Doesn't have attribute \"%s\"", attributeName));
+        }
+        return attributes.get(sanitizeAttributeName(attributeName));
+    }
+    
+    /**
+     * 
+     * @param attributeName the name of the attribute to get
+     * @return the attribute with the given name
+     * @throws NullPointerException if this has no such attribute with the given name
+     */
+    public final String getAttributeString(String attributeName) throws NullPointerException {
+        return getAttribute(attributeName).toString();
+    }
+    
+    /**
+     * 
+     * @param attributeName the attribute to get
+     * @return the given attribute, as a double
+     * @throws NumberFormatException if the given attribute is not a number
+     */
+    public final double getAttributeDouble(String attributeName) throws NumberFormatException {
+        String s = getAttributeString(attributeName);
+        double ret = 0.0;
+        try {
+            ret = Double.valueOf(s);
+        } catch(NumberFormatException ex){
+            Logger.logError(String.format("Attribute with name \"%s\" cannot be formatted as a number. Its value is \"%s\"", attributeName, s));
+            throw ex;
+        }
+        return ret;
+    }
+    public final int getAttributeInt(String attributeName){
+        return (int)getAttributeDouble(attributeName);
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Spreadsheet Record:");
+        attributes.forEach((k, v)->{
+            sb.append(String.format("\n- %s : %s", k, v.toString()));
+        });
+        return sb.toString();
+    }
+}
