41404de10677a9e2c6eea21377b1b8b7dfdb3c5d
IronHeart7334 <greengrappler12@gmail.com>
2020-10-13 09:56:46 -0700

    split excel parser. At some point, I'll have to choose where I want to use inheritance, and where I want to just use generics and functions

--- src/main/java/autocadDrawingChecker/data/autoCADData/AutoCADExcelParser.java
+++ src/main/java/autocadDrawingChecker/data/autoCADData/AutoCADExcelParser.java
@@ -1,19 +1,9 @@
 package autocadDrawingChecker.data.autoCADData;
 
-import autocadDrawingChecker.logging.Logger;
-import java.io.FileInputStream;
+import autocadDrawingChecker.data.core.ExcelParser;
+import autocadDrawingChecker.data.core.ExtractedSpreadsheetContents;
+import autocadDrawingChecker.data.core.RecordExtractor;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  * The AutoCADExcelParser is used to 
@@ -23,110 +13,24 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  * 
  * @author Matt Crow
  */
-public class AutoCADExcelParser {
-    private final String fileName;
-    private final HashMap<String, Integer> headerToCol;
-    private Row currRow;
+public class AutoCADExcelParser extends ExcelParser {
+    
     
     /**
      * @param fileToParse a path to the Excel file this should parse.
      */
     public AutoCADExcelParser(String fileToParse){
-        fileName = fileToParse;
-        headerToCol = new HashMap<>();
-        currRow = null;
+        super(fileToParse);
     }
     
-    /**
-     * Locates headers within the given
-     * row, and populates headerToCol appropriately.
-     * 
-     * @param headerRow the first row of the spreadsheet,
-     * containing headers.
-     */
-    private synchronized void locateColumns(Row headerRow){
-        headerToCol.clear();
-        ArrayList<String> headers = new ArrayList<>();
-        headerRow.cellIterator().forEachRemaining((Cell c)->{
-            headers.add(c.toString().toUpperCase());
-        });
-        for(int i = 0; i < headers.size(); i++){
-            headerToCol.put(headers.get(i), i);
-        }
+    @Override
+    protected ExtractedSpreadsheetContents createExtractionHolder(){
+        return new AutoCADExport(this.getFileName());
     }
     
-    private boolean isRowEmpty(Row row){
-        boolean couldBeEmpty = true;
-        Iterator<Cell> cellIter = row.cellIterator();
-        while(cellIter.hasNext() && couldBeEmpty){
-            if(!cellIter.next().getCellType().equals(CellType.BLANK)){
-                couldBeEmpty = false;
-            }
-        }
-        return couldBeEmpty;
-    }
-    private boolean isValidRow(Row row){
-        return row != null && row.getLastCellNum() != -1 && !isRowEmpty(row);
-    }
-    
-    private String currRowToString(){
-        StringBuilder b = new StringBuilder();
-        b.append("[");
-        Iterator<Cell> iter = currRow.cellIterator();
-        while(iter.hasNext()){
-            b.append(iter.next().toString());
-            if(iter.hasNext()){
-                b.append(", ");
-            }
-        }
-        b.append("]");
-        return b.toString();
-    }
-    
-    public final AutoCADExport parse() throws IOException {
-        InputStream in = new FileInputStream(fileName);
-        //                                                new Excel format       old Excel format
-        Workbook workbook = (fileName.endsWith("xlsx")) ? new XSSFWorkbook(in) : new HSSFWorkbook(in);
-        Sheet sheet = workbook.getSheetAt(0);
-        AutoCADExport containedTherein = new AutoCADExport(fileName);
-        locateColumns(sheet.getRow(0));
-        
-        
-        AutoCADElementExtractor recExtr = new AutoCADElementExtractor();
-        
-        int numRows = sheet.getLastRowNum() + 1; // need the + 1, otherwise it sometimes doesn't get the last row
-        /*
-        Note that numRows will be greater than or
-        equal to the last row with data, but not
-        every row is guaranteed to contain data.
-        
-        From the Apache POI javadoc:
-        """
-        Gets the last row on the sheet 
-        Note: rows which had content before and were set to empty later might still be counted as rows by Excel and Apache POI, 
-        so the result of this method will include such rows and thus the returned value might be higher than expected!
-        """
-        */
-        
-        AutoCADElement rec = null;
-        
-        //               skip headers
-        for(int rowNum = 1; rowNum < numRows; rowNum++){
-            currRow = sheet.getRow(rowNum);
-            if(isValidRow(currRow)){
-                try {
-                    rec = (AutoCADElement)recExtr.extract(headerToCol, currRow);
-                    containedTherein.add(rec);
-                } catch(Exception ex){
-                    Logger.logError(String.format("Error while parsing row: %s", currRowToString()));
-                    Logger.logError(ex);
-                }
-            }
-        }
-        //Logger.log("In AutoCADExcelParser.parse...\n" + containedTherein.toString());
-        
-        workbook.close();
-        return containedTherein;
+    @Override
+    protected RecordExtractor createExtractor(){
+        return new AutoCADElementExtractor();
     }
     
     /**
@@ -143,7 +47,7 @@ public class AutoCADExcelParser {
      * @throws IOException if the fileName given does not point to an Excel file
      */
     public static AutoCADExport parse(String fileName) throws IOException{
-        return new AutoCADExcelParser(
+        return (AutoCADExport)new AutoCADExcelParser(
             fileName
         ).parse();
     }
