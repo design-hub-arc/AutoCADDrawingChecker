a9bbe8dcc9a140c4e588ac51e4ba75319900e5b8
IronHeart7334 <greengrappler12@gmail.com>
2020-11-12 10:02:21 -0800

    Not sure if this is too big to commit, but I'll see.

--- src/main/java/autocadDrawingChecker/start/Main.java
+++ src/main/java/autocadDrawingChecker/start/Main.java
@@ -1,10 +1,7 @@
 package autocadDrawingChecker.start;
 
 import autocadDrawingChecker.data.GradableDataTypeLoader;
-import autocadDrawingChecker.data.csv.CsvParser;
-import autocadDrawingChecker.data.excel.surveyData.SurveyDataParser;
 import autocadDrawingChecker.grading.criteria.GradingCriteriaLoader;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -24,7 +21,6 @@ public class Main {
     
     /**
      * @param args the command line arguments.
-     * How do I set these in Netbeans?
      * 
      * To pass arguments to Gradle, use
      * 
@@ -46,23 +42,7 @@ public class Main {
         boolean noGui = argSet.contains("--no-gui");
         
         if(debug){
-            try {
-                new SurveyDataParser().parseAllSheets("C:\\Users\\Matt\\Desktop\\AutoCAD Drawing Checker\\sample files to work with\\Survey Data\\template\\GPS Style - Survey Field Notes - Template.xlsx").forEach(System.out::println);
-                new CsvParser(true).parseFirstSheet("C:\\Users\\Matt\\Desktop\\AutoCAD Drawing Checker\\sample files to work with\\Csv Data\\nodeCoords.csv").forEach(System.out::println);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-            /*
-            data
-            .setInstructorFilePath("C:\\Users\\Matt\\Desktop\\AutoCAD Drawing Checker\\sample files to work with\\Survey Data\\civil67\\67Civi-Student 1 - GPS Style Survey Simulation - Survey Field Notes.xlsx")
-              .setStudentFilePaths("C:\\Users\\Matt\\Desktop\\AutoCAD Drawing Checker\\sample files to work with\\Survey Data\\civil67");
-            for(double d = 0.0; d < 1000.0; d += 100.0){
-                data.setCriteriaThreshold(d);
-                System.out.printf("\nWhen threshold is %f...\n", d);
-                data.grade().forEach((gradedExport)->{
-                    System.out.printf("%5.3f ", gradedExport.getFinalGrade());
-                });
-            }*/
+            // insert your debug code here!
         } 
         
         if(!noGui){
